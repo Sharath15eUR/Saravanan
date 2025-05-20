4a88cb2afb8905682ed0fb2bbf952ce93b369796
saravanan <saravanan16102003@gmail.com>
2025-05-20 20:32:39 +0530

    mod 2

---
+++ Desktop/advanced_C_mod2/Q2_signal.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<pthread.h>
+#include <time.h>
+#include <signal.h>
+
+int N;
+void sigint_handler(int sig) {
+    printf("\nSIGINT (Ctrl + C) received, program won't terminate!\n");
+}
+
+
+ int is_prime(int num){
+ if (num<2) return 0;
+ for(int i=2;i*i<=num;i++){
+ if(num%i==0) return 0;
+ }
+ return 1;
+ }
+ 
+ void * threadfuncA(void *arg){
+ int count=0, sum=0,num=2;
+ while(count<N){
+ if(is_prime(num)){
+ sum+=num;
+ count++;
+ }
+ num++;
+ }
+ printf("the sum of first %d prime numbers is:%d\n",N,sum);
+ return NULL;
+ }
+ void * threadfuncB(void *arg){
+ int time=100,t=0;
+ while(t<time){
+ printf("thread 2 is running!\n");
+ sleep(2);
+ t+=2;
+ }
+ return NULL;
+}
+ void * threadfuncC(void *arg){
+ int time=100,t=0;
+ while(t<time){
+ printf("thread 3 is running!\n");
+ sleep(3);
+ t+=3;
+ } return NULL;
+ 
+ }
+ int main(){
+ printf("enter the number ?");
+ scanf("%d",&N);
+ signal(SIGINT, sigint_handler);  
+ clock_t start = clock();         
+
+ pthread_t ta,tb,tc;
+ pthread_create(&ta,NULL,threadfuncA,NULL);
+ pthread_create(&tb,NULL,threadfuncB,NULL);
+ pthread_create(&tc,NULL,threadfuncC,NULL);
+ 
+ pthread_join(ta,NULL);
+ pthread_join(tb,NULL);
+ pthread_join(tc,NULL);
+ clock_t end = clock();          
+ double time_taken = (double)(end - start) ;
+ printf("Time taken: %f seconds\n", time_taken);
+ 
+ return 0;
+ 
+ }
+ 
